5540da98592bc2cc88363151ef23e38c9d117139
Franz <shedali@gmail.com>
2025-08-14 16:08:12 +0100

    chore: remove server

--- index.ts
+++ index.ts
@@ -3,12 +3,3 @@
 console.log("Hello from Bun!");
 console.log(`Bun version: ${Bun.version}`);
 
-// Simple HTTP server example
-const server = Bun.serve({
-  port: 3000,
-  fetch(req) {
-    return new Response("Hello World from Bun server!");
-  },
-});
-
-console.log(`Server running at http://localhost:${server.port}`);
