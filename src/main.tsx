3c4fadda63a9e8913b8869aa53426eedd60f90af
Franz <shedali@gmail.com>
2025-08-18 18:36:02 +0100

    update docs

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,12 @@ function App() {
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
       <p>Nix template</p>
+      <ul>
+        <li>security scanning</li>
+        <li>dependabot</li>
+        <li>cachix binary cache</li>
+        <li>commit hooks</li>
+      </ul>
     </div>
   );
 }
